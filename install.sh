b6392e65df2605511ae4ebbb4cabf5ddf567b8b4
purans939 <ps939@njit.edu>
2023-11-30 20:38:55 -0500

    created base github actions workflow template

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
             
-            sudo yum install git -y
+sudo yum install git -y
             
-            # 1. cloning chatboi-ui from git repo
-            mkdir -p /usr/gitrepo/
-            git clone https://github.com/mckaywrigley/chatbot-ui /usr/gitrepo/
+# 1. cloning chatboi-ui from git repo
+mkdir -p /usr/gitrepo/
+git clone https://github.com/mckaywrigley/chatbot-ui /usr/gitrepo/
 
-            # 2. install dependencies - sudo apt install npm
-            npm i
+# 2. install dependencies - sudo apt install npm
+npm i
 
-            # 3. provide OpenAI API Key: OPENAI_API_KEY=YOURKEY in .env.local file in root of repo
+# 3. provide OpenAI API Key: OPENAI_API_KEY=YOURKEY in .env.local file in root of repo
 
-            # 4. run app
-            npm run dev
+# 4. run app
+npm run dev
